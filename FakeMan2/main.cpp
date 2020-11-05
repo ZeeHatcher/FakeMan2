2f9de669535d6c695b5842c64702e5d8b7c3a04b
Jonathan Seng <zee.hatcher010@gmail.com>
2020-11-06 00:42:35 +0800

    fix main

--- FakeMan2/main.cpp
+++ FakeMan2/main.cpp
@@ -1,36 +1,12 @@
 #include <iostream>
 #include "splashkit.h"
 #include "Game.h"
-#include "GameObjectTest.h"
 
 int main()
 {
 
-    open_graphics_window("FakeMan", WINDOW_WIDTH, WINDOW_HEIGHT);
+    open_window("FakeMan", WINDOW_WIDTH, WINDOW_HEIGHT);
     // show_swin_game_splash_screen();
-
-	// ****************
-	// ** Unit Tests **
-	// ****************
-	std::cout << "---| GameObjectTest |---" << endl;
-
-	// Tests constructor for derived classes and tests if they are derived from GameObject
-	GameObjectTest::testAmmoConstructor();
-	GameObjectTest::testBombConstructor();
-	GameObjectTest::testCellConstructor();
-	GameObjectTest::testEnemyConstructor();
-	GameObjectTest::testExplosionConstructor();
-	GameObjectTest::testPlayerConstructor();
-	GameObjectTest::testWallConstructor();
-
-	// Tests getBounding() function of derived classes
-	GameObjectTest::testAmmoGetBounding();
-	GameObjectTest::testBombGetBounding();
-	GameObjectTest::testCellGetBounding();
-	GameObjectTest::testEnemyGetBounding();
-	GameObjectTest::testExplosionGetBounding();
-	GameObjectTest::testPlayerGetBounding();
-	GameObjectTest::testWallGetBounding();
 
 	// **********
 	// ** Main **
@@ -41,16 +17,13 @@ int main()
     {
         process_events();
         
-        clear_screen(ColorBlack);
+        clear_screen(COLOR_BLACK);
         
         game.update();
         game.draw();
-		
-		draw_framerate(0,0);
         
         refresh_screen(60);
-    } while ( ! window_close_requested() );
+    } while (!quit_requested());
     
-    release_all_resources();
     return 0;
 }
