fb9e63c166f9bee4ec48ff0e6db757d8f2699617
Jonathan Seng <zee.hatcher010@gmail.com>
2020-11-12 18:27:35 +0800

    cleanup

--- test/GameObjectTest.h
+++ test/GameObjectTest.h
@@ -6,16 +6,8 @@ class GameObjectTest
 {
 public :
 	static void testAmmoConstructor();
-
-public :
 	static void testBombConstructor();
-
-public: 
 	static void testCellConstructor();
-
-public:
 	static void testEnemyConstructor();
-
-public:
 	static void testExplosionConstructor();
 };
