62f9a53a6aa4d270d28528400d34c0d4dc99e12a
Cherry <tohhie00@gmail.com>
2020-11-12 18:10:57 +0800

    Fixed error bc I'm careless in test

--- test/GameObjectTest.h
+++ test/GameObjectTest.h
@@ -1,21 +1,21 @@
 #pragma once
 #include <iostream>
 #include "Ammo.h"
+#include "Bomb.h"
+#include "Enemy.h"
+#include "Cell.h"
+#include "Explosion.h"
 
 class GameObjectTest
 {
 public :
 	static void testAmmoConstructor();
 
-public :
 	static void testBombConstructor();
 
-public: 
 	static void testCellConstructor();
 
-public:
 	static void testEnemyConstructor();
 
-public:
 	static void testExplosionConstructor();
 };
