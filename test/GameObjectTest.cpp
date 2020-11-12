0f459a2167e8634d16255e539e81f09d4baa0c4a
Cherry <tohhie00@gmail.com>
2020-11-12 17:45:11 +0800

    More unit test added

--- test/GameObjectTest.cpp
+++ test/GameObjectTest.cpp
@@ -50,3 +50,37 @@ void GameObjectTest::testCellConstructor()
 		std::cout << "Failed" << std::endl;
 	}
 }
+
+void GameObjectTest::testEnemyConstructor()
+{
+	std::cout << "testEnemyConstructor: ";
+
+	try
+	{
+		Enemy enemy(10, 20, ColorBlack, 1, 2);
+		GameObject obj = (GameObject)enemy;
+
+		std::cout << "Successful" << std::endl;
+	}
+	catch (...)
+	{
+		std::cout << "Failed" << std::endl;
+	}
+}
+
+void GameObjectTest::testExplosionConstructor()
+{
+	std::cout << "testExplosionConstructor: ";
+
+	try
+	{
+		Explosion explosion(10, 20);
+		GameObject obj = (GameObject)explosion;
+
+		std::cout << "Successful" << std::endl;
+	}
+	catch (...)
+	{
+		std::cout << "Failed" << std::endl;
+	}
+}
--- test/main.cpp
+++ test/main.cpp
@@ -7,4 +7,6 @@ int main()
   runner.testAmmoConstructor();
   runner.testBombConstructor();
   runner.testCellConstructor();
+  runner.testEnemyConstructor();
+  runner.testExplosionConstructor();
 }
