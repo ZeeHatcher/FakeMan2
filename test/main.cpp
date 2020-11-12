8eb3c301d479447b1eb4284d36139295af813fdb
Jonathan Seng <zee.hatcher010@gmail.com>
2020-11-12 17:56:54 +0800

    fix workflow

--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "splashkit.h"
 #include "Game.h"
+#include "GameObjectTest.h"
 
 int main()
 {
+  GameObjectTest runner;
+
   runner.testAmmoConstructor();
   runner.testBombConstructor();
   runner.testCellConstructor();
